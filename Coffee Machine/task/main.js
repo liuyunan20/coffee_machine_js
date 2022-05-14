4d7c6da1ab5d4fab532546d8d3b09d19078943bf
liuyunan <liuyunan.20@gmail.com>
2022-05-14 00:33:51 -0700

    Write a program that does the following:
    It requests the amounts of water, milk, and coffee beans available at the moment and then asks for the number of required coffee cups;
    If the coffee machine has the supplies to make enough coffee, the program should print the following: Yes, I can make that amount of coffee.
    If the coffee machine can make more than that, the program should output Yes, I can make that amount of coffee (and even N more than that), where N is the number of extra cups.
    If you're running short, the program should output No, I can make only N cups of coffee.
    Like in the previous stage, the coffee machine needs 200 ml of water, 50 ml of milk, and 15 g of coffee beans for one cup.

--- Coffee Machine/task/main.js
+++ Coffee Machine/task/main.js
@@ -1,12 +1,19 @@
 // Use "input()" to input a line from the user
 // Use "input(str)" to print some text before requesting input
 // You will need this in the following stages
-//const input = require('sync-input')
+const input = require('sync-input')
+//import input from "sync-input";
 
-console.log("Starting to make a coffee")
-console.log("Grinding coffee beans")
-console.log("Boiling water")
-console.log("Mixing boiled water with crushed coffee beans")
-console.log("Pouring coffee into the cup")
-console.log("Pouring some milk into the cup")
-console.log("Coffee is ready!")
+
+let water = input("Write how many ml of water the coffee machine has:");
+let milk = input("Write how many ml of milk the coffee machine has:");
+let cofBean = input("Write how many grams of coffee beans the coffee machine has:");
+let cupNeed = input("Write how many cups of coffee you will need:");
+let cups = Math.floor(Math.min(water/200, milk/50, cofBean/15));
+if (cups == cupNeed) {
+    console.log("Yes, I can make that amount of coffee");
+} else if (cups > cupNeed) {
+    console.log(`Yes, I can make that amount of coffee (and even ${cups - cupNeed} more than that)`);
+} else {
+    console.log(`No, I can make only ${cups} cups of coffee`);
+}
