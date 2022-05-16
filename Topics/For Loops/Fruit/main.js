b7923a9bd493a3f43422feff493899cf1e4b52cf
liuyunan <liuyunan.20@gmail.com>
2022-05-16 11:53:23 -0700

    Write a function myFunc() where you create a counter variable i and use it inside the for loop. In the loop, print out all the cubes of numbers from 1 to 5.
    Think about which kind of identifier is more appropriate here.
    
    Also, make sure your code defines the function but doesn't call it.

--- Topics/For Loops/Fruit/main.js
+++ Topics/For Loops/Fruit/main.js
@@ -1,4 +1,8 @@
-let fruits = ['apple', 'orange', 'banana'];
-for (x in fruits) {
-    console.log(fruits[x]);
+let n = 0;
+let k = 10;
+let res = 0;
+for (; n < k; n++) {
+    res += n + k;
+    k--;
 }
+console.log(res);
