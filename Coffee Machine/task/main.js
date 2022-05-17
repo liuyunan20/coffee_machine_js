8e25667fb0a278391bdb856c4b4af3560c10e873
liuyunan <liuyunan.20@gmail.com>
2022-05-17 00:32:12 -0700

    Write a program that offers to buy one cup of coffee, to fill the supplies, or to take the money from the machine. Note that the program is supposed to do only one of the mentioned actions at a time. It should also calculate the amounts of the remaining ingredients and how much money is in store. Display the number of supplies before and after a purchase.
    Your program reads one option from the standard input that is either buy, fill, or take. If users want to buy a cup of coffee, the correct command is buy. If a special person thinks that it is time to top up the supplies, the command is fill. If another special worker decides that it is time to take the money from the coffee machine, input take.
    If users choose buy, they need to pick one of the three coffee types: an espresso, a latte, or a cappuccino. Enumerate the options as follows: 1 - espresso, 2 - latte, 3 - cappuccino. In addition to that, take into account the amounts of supplies for each variation:
    One espresso requires 250 ml of water and 16 g of coffee beans. It costs $4;
    One latte requires 350 ml of water, 75 ml of milk, and 20 g of coffee beans. It costs $7;
    One cappuccino requires 200 ml of water, 100 ml of milk, and 12 g of coffee beans. It costs $6.
    If users write fill, the program should ask them how much water, milk, coffee, and how many disposable cups they want to add to the coffee machine.
    If users write take, the program should give all the money that it has earned.
    Name the actions as buy, fill, and take.
    
    For a start, the coffee machine has $550, 400 ml of water, 540 ml of milk, 120 g of coffee beans, and 9 disposable cups.

--- Coffee Machine/task/main.js
+++ Coffee Machine/task/main.js
@@ -2,18 +2,74 @@
 // Use "input(str)" to print some text before requesting input
 // You will need this in the following stages
 const input = require('sync-input')
-//import input from "sync-input";
 
 
-let water = input("Write how many ml of water the coffee machine has:");
-let milk = input("Write how many ml of milk the coffee machine has:");
-let cofBean = input("Write how many grams of coffee beans the coffee machine has:");
-let cupNeed = input("Write how many cups of coffee you will need:");
-let cups = Math.floor(Math.min(water/200, milk/50, cofBean/15));
-if (cups == cupNeed) {
-    console.log("Yes, I can make that amount of coffee");
-} else if (cups > cupNeed) {
-    console.log(`Yes, I can make that amount of coffee (and even ${cups - cupNeed} more than that)`);
+let water = 400;
+let milk = 540;
+let cofBean = 120;
+let cups = 9;
+let money = 550;
+
+function displaySupplies(w, m, cof, cup, mon) {
+    console.log(`The coffee machine has:
+${w} ml of water
+${m} ml of milk
+${cof} g of coffee beans
+${cup} disposable cups
+$${mon} of money`);
+}
+
+function buy() {
+    let coffee = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+    if (coffee == '1') {
+        water = water - 250;
+        cofBean = cofBean - 16;
+        cups--;
+        money = money + 4;
+    } else if (coffee == '2') {
+        water = water - 350;
+        milk = milk - 75;
+        cofBean = cofBean - 20;
+        cups--;
+        money = money + 7;
+    } else if (coffee == '3') {
+        water = water - 200;
+        milk = milk - 100;
+        cofBean = cofBean - 12;
+        cups--;
+        money = money + 6;
+    } else {
+        console.log('Wrong order');
+    }
+}
+
+function fill() {
+    let w = input("Write how many ml of water you want to add:");
+    let m = input("Write how many ml of milk you want to add:");
+    let cof = input("Write how many grams of coffee beans you want to add:");
+    let cup = input("Write how many disposable coffee cups you want to add:");
+    water = water + Number(w);
+    milk = milk + Number(m);
+    cofBean = cofBean + Number(cof);
+    cups = cups + Number(cup);
+}
+
+function take() {
+    console.log(`I gave you $${money}`);
+    money = 0;
+}
+
+displaySupplies(water, milk, cofBean, cups, money);
+let action = input("Write action (buy, fill, take):");
+if (action == 'buy') {
+    buy();
+    displaySupplies(water, milk, cofBean, cups, money);
+} else if (action == 'fill') {
+    fill();
+    displaySupplies(water, milk, cofBean, cups, money);
+} else if (action == 'take') {
+    take();
+    displaySupplies(water, milk, cofBean, cups, money);
 } else {
-    console.log(`No, I can make only ${cups} cups of coffee`);
+    console.log('Wrong action');
 }
