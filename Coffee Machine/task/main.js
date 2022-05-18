ed0fdad32cbde7b758c55873aaa09d99c0e5324f
liuyunan <liuyunan.20@gmail.com>
2022-05-18 11:09:37 -0700

    add an option to choose large size

--- Coffee Machine/task/main.js
+++ Coffee Machine/task/main.js
@@ -7,71 +7,34 @@ const input = require('sync-input')
 let water = 400;
 let milk = 540;
 let cofBean = 120;
-let cups = 9;
+let cups = {'regular': 9, 'large': 9};
 let money = 550;
+const espresso = {'water': 250, 'milk': 0, 'bean': 16, 'money': 4};
+const latte = {'water': 350, 'milk': 75, 'bean': 20, 'money': 7};
+const cappuccino = {'water': 200, 'milk': 100, 'bean': 12, 'money': 6};
 
 function displaySupplies(w, m, cof, cup, mon) {
     console.log(`The coffee machine has:
 ${w} ml of water
 ${m} ml of milk
 ${cof} g of coffee beans
-${cup} disposable cups
+${cup.regular} regular disposable cups
+${cup.large} large disposable cups
 $${mon} of money`);
 }
 
 function buy() {
     let coffee = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+    let size = Number(input("what size do want? 1 - regular, 2 - large"))
     switch (coffee) {
         case '1':
-            if (water < 250) {
-                console.log('Sorry, not enough water!');
-            } else if (cofBean < 16) {
-                console.log('Sorry, not enough coffee beans!');
-            } else if (cups === 0) {
-                console.log('Sorry, not enough cups!');
-            } else {
-                water = water - 250;
-                cofBean = cofBean - 16;
-                cups--;
-                money = money + 4;
-                console.log('I have enough resources, making you a coffee!')
-            }
+            makeCoffee(espresso, size)
             break;
         case '2':
-            if (water < 350) {
-                console.log('Sorry, not enough water!');
-            } else if (milk < 75){
-                console.log('Sorry, not enough milk!');
-            } else if (cofBean < 20) {
-                console.log('Sorry, not enough coffee beans!');
-            } else if (cups === 0) {
-                console.log('Sorry, not enough cups!');
-            } else {
-                water = water - 350;
-                milk = milk - 75;
-                cofBean = cofBean - 20;
-                cups--;
-                money = money + 7;
-                console.log('I have enough resources, making you a coffee!')
-            }
+            makeCoffee(latte, size)
             break;
         case '3':
-            if (water < 200) {
-                console.log('Sorry, not enough water!');
-            } else if (milk < 100){
-                console.log('Sorry, not enough milk!');
-            } else if (cofBean < 12) {
-                console.log('Sorry, not enough coffee beans!');
-            } else if (cups === 0) {
-                console.log('Sorry, not enough cups!');
-            } else {
-                water = water - 200;
-                milk = milk - 100;
-                cofBean = cofBean - 12;
-                cups--;
-                money = money + 6;
-                console.log('I have enough resources, making you a coffee!')
-            }
+            makeCoffee(cappuccino, size)
             break;
     }
 }
@@ -92,10 +55,33 @@ function take() {
     money = 0;
 }
 
+function makeCoffee(coffee, size) {
+    if (water < coffee.water * size) {
+        console.log('Sorry, not enough water!');
+    } else if (milk < coffee.milk * size) {
+        console.log('Sorry, not enough milk!');
+    } else if (cofBean < coffee.bean * size) {
+        console.log('Sorry, not enough coffee beans!');
+    } else if ((size === 1 && cups.regular === 0) || (size === 2 && cups.large === 0)) {
+        console.log('Sorry, not enough cups!');
+    } else {
+        water = water - coffee.water * size;
+        milk = milk - coffee.milk * size;
+        cofBean = cofBean - coffee.bean * size;
+        if (size === 1) {
+            cups.regular--;
+        } else if (size === 2) {
+            cups.large--;
+        }
+        money = money + coffee.money * size * 0.75;
+        console.log('I have enough resources, making you a coffee!')
+    }
+}
+
 
 let action = null;
 while (action !== 'exit') {
-    action = input("Write action (buy, fill, take):");
+    action = input("Write action (buy, fill, take, remaining, exit):");
     switch (action){
         case 'buy':
             buy();
